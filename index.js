d12516081fbd41da661f4bc67aa929e6e5bbb0d4
0xsatwik <airdropking1810@gmail.com>
2025-06-09 12:03:08 +0530

    Refactor commit generation logic to create 127 random commits starting from February 1st, 2025. Improved error handling during file writing and committing processes.

--- index.js
+++ index.js
@@ -1,40 +1,63 @@
 import jsonfile from "jsonfile";
 import moment from "moment";
 import simpleGit from "simple-git";
-import random from "random";
 
 const path = "./data.json";
 
-const markCommit = (x, y) => {
-  const date = moment()
-    .subtract(1, "y")
-    .add(1, "d")
-    .add(x, "w")
-    .add(y, "d")
-    .format();
+const makeCommits = (dates) => {
+  if (dates.length === 0) {
+    simpleGit().push((err) => {
+      if (err) {
+        console.error("Error on push:", err);
+      } else {
+        console.log("All commits pushed successfully.");
+      }
+    });
+    return;
+  }
+
+  const [currentDate, ...remainingDates] = dates;
+  const formattedDate = currentDate.format();
 
   const data = {
-    date: date,
+    date: formattedDate,
   };
 
-  jsonfile.writeFile(path, data, () => {
-    simpleGit().add([path]).commit(date, { "--date": date }).push();
+  jsonfile.writeFile(path, data, (err) => {
+    if (err) {
+      console.error(`Error writing file for date ${formattedDate}:`, err);
+      // Skip this date and move to the next
+      makeCommits(remainingDates);
+      return;
+    }
+
+    simpleGit()
+      .add([path])
+      .commit(formattedDate, { "--date": formattedDate }, (err) => {
+        if (err) {
+          console.error(`Error committing for date ${formattedDate}:`, err);
+        } else {
+          console.log(`Committed for ${formattedDate}`);
+        }
+        // Whether commit succeeded or failed, move to the next date
+        makeCommits(remainingDates);
+      });
   });
 };
 
-const makeCommits = (n) => {
-  if(n===0) return simpleGit().push();
-  const x = random.int(0, 54);
-  const y = random.int(0, 6);
-  const date = moment().subtract(1, "y").add(1, "d").add(x, "w").add(y, "d").format();
+// Generate 127 consecutive days starting from February 1st, 2025
+const startDate = moment("2025-02-01");
+const dates = [];
+for (let i = 0; i < 127; i++) {
+  dates.push(startDate.clone().add(i, "days"));
+}
 
-  const data = {
-    date: date,
-  };
-  console.log(date);
-  jsonfile.writeFile(path, data, () => {
-    simpleGit().add([path]).commit(date, { "--date": date },makeCommits.bind(this,--n));
-  });
-};
+// Shuffle the array of dates to make commit order random
+for (let i = dates.length - 1; i > 0; i--) {
+  const j = Math.floor(Math.random() * (i + 1));
+  [dates[i], dates[j]] = [dates[j], dates[i]];
+}
 
-makeCommits(100);
+// Start the commit process
+console.log("Starting to make 127 commits...");
+makeCommits(dates);
